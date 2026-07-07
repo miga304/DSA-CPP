859333657d3fd4a94560a2b2e7f16d39e80e4a0e
AnamikaBhat <anamikabhat304@gmail.com>
2026-07-07 16:01:39 +0530

    Refactor merge function for merging sorted arrays

--- 01-two-pointers/Merge_Sorted_Array.cpp
+++ 01-two-pointers/Merge_Sorted_Array.cpp
@@ -25,28 +25,35 @@ in nums1, allowing an in-place solution.
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int r1=m-1;
+        int r2=n-1;
+        int i=m+n-1;
 
-        // Handle edge case
-        if(nums1.empty())nums1=nums2;
-      
-        int p1 = m-1;
-        int p2 = n-1;
-        int p = m+n-1;
-
-        while(p1 >= 0 && p2 >= 0)
+        while(r1 >= 0 && r2 >=0)
         {
-            if(nums2[p2] > nums1[p1])
+            if(nums1[r1] > nums2[r2])
             {
-                nums1[p]=nums2[p2];
-                p--;
-                p2--;
+                nums1[i]=nums1[r1];
+                r1--;
             }
             else
             {
-                nums1[p] = nums1[p1];
-                p1--;
-                p--;
+                nums1[i]=nums2[r2];
+                r2--;
             }
-        }     
+            i--;
+        }
+
+        while(r1 >=0 )
+        {
+            nums1[i]=nums1[r1];
+                r1--;i--;
+        }
+        while(r2 >=0 )
+        {
+            nums1[i]=nums2[r2];
+                r2--;i--;
+        }
+        
     }
 };
